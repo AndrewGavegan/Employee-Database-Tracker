fff451c88853d02c8b49eb02ff0bb1dd09768f03
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-05-16 17:29:21 +1000

    testing inquirer

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const inquirer = require('inquirer');
 
 
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 
 // creating a connection to the database //
@@ -80,18 +80,138 @@ function callQuestions() {
             }
     })
 }
+// function for adding new employees //
+function emplyName() {
+    return ([
+        {
+                name: "firstname",
+                type: "input",
+                message: "What is the employees first name?"
+        },
+        {
+            name: "lastname",
+            type: "input",
+            message: "What is the employees last name?"
+        }
+    ]);
+}
 
 // now to write the functions that I called above in the inquirer answer function //
-function viewDept() {}
+function viewDept() {
+    // paste in code from query.sql file in template literals//
+    const departments = `SELECT department.id, department.department_name
+    FROM department 
+    ORDER BY department.id;`;
+// select all the records from department table and display the result //
+    db.query(departments, (err, res) => {
+        if (err) throw err;
+        console.log('\n Departments Table \n')
+        console.table(res);
+        // call the inquirer to ask questions again //
+        callQuestions();
+    });
+}
 
-function viewRole() {}
+function viewRole() {
+    // paste in code from query.sql file in template literals//
+    const roles = `SELECT roles.id, roles.title AS roles, roles.salary AS  salary, department.department_name AS department
+    FROM roles 
+    LEFT JOIN department
+    ON roles.department_id = department.id
+    ORDER BY roles.id;`;
+// select all the records from roles table and display the result //
+    db.query(roles, (err, res) => {
+        if (err) throw err;
+        console.log('\n Roles Table \n')
+        console.table(res);
+        // call the inquirer to ask questions again //
+        callQuestions();
+    });
+}
 
-function viewEmply() {}
+function viewEmply() {
+     // paste in code from query.sql file in template literals//
+     const employee = `SELECT  employee.id, employee.first_name, employee.last_name, roles.title, 
+     department.department_name AS department, roles.salary, 
+     CONCAT(manager.first_name, ' ', manager.last_name) AS manager    
+     FROM employee
+     LEFT JOIN employee manager ON manager.id = employee.managers_id
+     INNER JOIN roles ON roles.id = employee.role_id
+     INNER JOIN department ON department.id = roles.department_id
+     ORDER BY employee.id;`;
+ // select all the records from employee table and display the result //
+     db.query(employee, (err, res) => {
+         if (err) throw err;
+         console.log('\n Employee Table \n')
+         console.table(res);
+         // call the inquirer to ask questions again //
+         callQuestions();
+     });
+}
 
 function addDept() {}
 
 function addRole() {}
 
-function addEmply() {}
+async function addEmply() {
+    const newEmply = await inquirer.prompt(emplyName());
+    db.query('SELECT roles.id, roles.title FROM roles ORDER BY roles.id;', async (err, res) => {
+        if (err) throw err;
+        const {roles} = await inquirer.prompt([
+            {
+                name: 'emplyRole',
+                type: 'list',
+                choices: () => res.map(res => res.title),
+                message: 'What role does this employee have?: '
+            }
+        ]);
+        let rolesId;
+        for (const row of res) {
+            if (row.title === roles) {
+                rolesId = row.id;
+                continue;
+            }
+        }
+        db.query('SELECT * FROM employee', async (err, res) => {
+            if (err) throw err;
+            let names = res.map(res => `${res.firstName} ${res.lastName}`);
+            names.push('nothing');
+            let { manager } = await inquirer.prompt([
+                {
+                    name: 'emplyManager',
+                    type: 'list',
+                    choices: names,
+                    message: 'Who will this employees manager be? '
+                }
+            ]);
+            let managerId;
+            let managerName;
+            if (emplyManager === 'nothing') {
+                managerId = null;
+                } else {
+                    for (const row of res) {
+                        row.mngrName = `${data.firstName} ${data.lastName}`;
+                        if (row.mngrName === manager){
+                            managerId = row.id;
+                            managerName = row.mngrName
+                            continue;
+                        }
+                    }
+                }
+            console.log('New employee added!');
+            db.query('INSERT INTO employee SET ?', {
+                first_name: newEmply.firstname,
+                last_name: newEmply.lastname,
+                role_id: rolesId,
+                managers_id: parseInt(managerId)
+            },
+            (err, res) => {
+                if (err) throw err;
+                callQuestions();
+                }
+            )
+        });
+    });
+}
 
 function updateEmply() {}
